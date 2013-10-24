41b8f027bdd40f1a60f970ad56a0b8bbddc0c0ce
matvey <mykola.mateychuk@internetdevels.com>
2013-10-24 11:31:27 +0300

    Install drupal admin menu sxd + bacup data base

---
+++ sxd/ses.php
@@ -0,0 +1,31 @@
+<?php
+$SES = array (
+  'b46dbbc9789b0cde266ba0c2ace15468' => 
+  array (
+    'cfg' => 
+    array (
+      'charsets' => 'cp1251 utf8 latin1',
+      'lang' => 'ru',
+      'time_web' => '600',
+      'time_cron' => '600',
+      'backup_path' => 'backup/',
+      'backup_url' => 'backup/',
+      'only_create' => 'MRG_MyISAM MERGE HEAP MEMORY',
+      'globstat' => 0,
+      'my_host' => 'localhost',
+      'my_port' => 3306,
+      'my_user' => 'vitryak',
+      'my_pass' => 'vitryak',
+      'my_comp' => 0,
+      'my_db' => '',
+      'auth' => 'mysql cfg',
+      'user' => '',
+      'pass' => '',
+      'confirm' => '6',
+      'exitURL' => './',
+    ),
+    'time' => 1382603267,
+    'lng' => 'ru',
+  ),
+);
+?>
